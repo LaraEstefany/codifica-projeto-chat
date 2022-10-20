d91952ef130d4c98a2bdfd8c15879fd3a7ec7d9f
Lara Costa <lara.estefanypc@gmail.com>
2022-10-19 22:27:11 -0300

    Primeiro commit.

---
+++ script.js
@@ -0,0 +1,32 @@
+chat = []
+
+var lista = document.getElementById("listaDeMensagens")
+
+function manda1() {
+    var campoMensagemUsuario1 = document.getElementById("mensagemUsuario1")
+    var mensagemUsuario1 = document.getElementById("mensagemUsuario1").value
+
+    chat.push(mensagemUsuario1)
+
+    var li = document.createElement("li")
+    li.className
+    li.innerText = mensagemUsuario1
+    lista.append(li)
+
+    campoMensagemUsuario1.value = ""
+}
+
+function manda2() {
+    var campoMensagemUsuario2 = document.getElementById("mensagemUsuario2")
+    var mensagemUsuario2 = document.getElementById("mensagemUsuario2").value
+
+    chat.push(mensagemUsuario2)
+
+    var li = document.createElement("li")
+    li.innerText = mensagemUsuario2
+    lista.append(li)
+
+    campoMensagemUsuario2.value = ""
+}
+
+// lista.innerHTML = `<div><p>${(chat[contador])}<\p><\div>`
