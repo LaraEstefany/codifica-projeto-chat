9c60b61656a32b30bb0b46aec42210a1f65683e8
Lara Costa <lara.estefanypc@gmail.com>
2022-10-24 18:34:07 -0300

    Mensagens anteriores aparecem na tela mas sem css.

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-chat = []
-
 var lista = document.getElementById("listaDeMensagens")
+var chat = []
+
+var chatArmazenado = localStorage.getItem("chat")
+chatArmazenado = JSON.parse(chatArmazenado)
+
+lista.append(chatArmazenado)
+
+// for (contagem = 0; chatArmazenado[contagem] = ((chatArmazenado.length) - 1); contagem++) {
+//     lista.append(chatArmazenado[contagem])
+// }
+//FAVOR IGNORAR ^^ MEU PC NÃO CONSEGUE RODAR O CÓDIGO PRA TESTAR
 
 function manda1() {
     var campoMensagemUsuario1 = document.getElementById("mensagemUsuario1")
     var mensagemUsuario1 = document.getElementById("mensagemUsuario1").value
-
-    chat.push(mensagemUsuario1)
 
     var li = document.createElement("li")
     li.classList.add("mensagemUsuario1")
     li.innerText = mensagemUsuario1
 
     lista.append(li)
+
+    chat.push(mensagemUsuario1) //chat.push(li)??
+
+    chatJSON = JSON.stringify(chat)
+    localStorage.setItem("chat", chatJSON)
 
     campoMensagemUsuario1.value = ""
 }
@@ -21,13 +33,18 @@ function manda2() {
     var campoMensagemUsuario2 = document.getElementById("mensagemUsuario2")
     var mensagemUsuario2 = document.getElementById("mensagemUsuario2").value
 
-    chat.push(mensagemUsuario2)
-
     var li = document.createElement("li")
     li.classList.add("mensagemUsuario2")
     li.innerText = mensagemUsuario2
 
     lista.append(li)
 
+    chat.push(mensagemUsuario2)
+
+    chatJSON = JSON.stringify(chat)
+    localStorage.setItem("chat", chatJSON)
+
     campoMensagemUsuario2.value = ""
 }
+
+// pegar direto do localStorage??
